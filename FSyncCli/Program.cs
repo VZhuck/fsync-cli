c2a25f2525323b714bf6540eb85e25ff96128182
valentine.zhuck <valentine.zhuck@gmail.com>
2020-07-21 11:34:59 -0400

    Init Prokect

---
+++ FSyncCli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+// System.CommandLine API auto conversion options with arguments into main (X,y) static method
+// System.CommandLine.DragonFruit
+// dotnet-suggest - support for tab completion.
+
+namespace FSyncCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
